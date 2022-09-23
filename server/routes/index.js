e026b3670df2fffb49ed74b4b4ee3fdbc3e84838
marlenerojas <marlene.rojas315@gmail.com>
2022-09-22 19:24:25 -0500

    Generador de cambios erroneos

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,10 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+const  router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 // al objeto que se le usa para llamar se llama view model
-  res.render('index', { 
-  title: 'Express' ,
-  author: 'Marlene Rodriguez'});
+  res.render('index', { title: 'Express', author: 'Marlene Rodriguez🤦‍♀️'});
 });
 
-module.exports = router;
+export default router;
