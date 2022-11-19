95d9fc791cc4326c2722690b3c8d244001e4a151
marlenerojas <marlene.rojas315@gmail.com>
2022-11-19 12:04:53 -0600

    Barra de Navegacion 📲

--- server/config/configKeys.js
+++ server/config/configKeys.js
@@ -5,39 +5,39 @@ import dotEnv from 'dotenv';
 dotEnv.config();
 
 //Creando un objeto de configuracion
-const defaultConfig = { 
-    port: process.env.port || 3000,
-    appVersion: process.env.APP_VERSION,
+const defaultConfig = {
+  port: process.env.port || 3000,
+  appVersion: process.env.APP_VERSION,
 };
 
-// Crear una configuracion para desarrollo 
+// Crear una configuracion para desarrollo
 const devConfig = {
-    env: "develoment",
-    mongoUrl: "local url",
-    debug: process.env.DEBUG,
+  env: 'develoment',
+  mongoUrl: 'local url',
+  debug: process.env.DEBUG,
 };
 
 // Configuracion para produccion
 const prodConfig = {
-    env: 'production',
-    mongoUrl: "cloud url",
+  env: 'production',
+  mongoUrl: 'cloud url',
 };
 
 //Funcion que dado en el entorno de ejecucion
 // nos retorne el objeto de configuracion adecuado
 function envConfig(env) {
-    switch (env) {
-        case 'development':
-            return devConfig;
-        case 'production':
-            return prodConfig;
-        default:
-            return prodConfig;
-    }
+  switch (env) {
+    case 'development':
+      return devConfig;
+    case 'production':
+      return prodConfig;
+    default:
+      return prodConfig;
+  }
 }
 
-// Exportar la configuracion 
+// Exportar la configuracion
 export default {
-    ...defaultConfig,
-    ...envConfig(process.env.NODE_ENV),
+  ...defaultConfig,
+  ...envConfig(process.env.NODE_ENV),
 };
