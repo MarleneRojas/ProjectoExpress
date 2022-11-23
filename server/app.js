76d0448d8c7d318fed5e0710f6ab04ccd1f10390
marlenerojas <marlene.rojas315@gmail.com>
2022-11-23 12:14:25 -0600

    Modelo Proyecto 📜

--- server/app.js
+++ server/app.js
@@ -28,14 +28,15 @@ import debug from './services/debugLogger';
 
 // Importando enrutador
 import router from './routes/router';
+
 // Importando los valores de entorno
 import configKeys from './config/configKeys';
 // Importando odm
 import MongooseOdm from './config/odm';
 
 // Recuperar el modo de ejecución de la app
-// const nodeEnv = process.env.NODE_ENV || 'development';
 const nodeEnv = configKeys.env;
+
 // Creando una instancia de express
 const app = express();
 
@@ -68,27 +69,28 @@ if (nodeEnv === 'development') {
   debug('✒ Ejecutando en modo de producción 🏭');
 }
 
-// Realizando la conexion a la base de datos
+// Realizando la conexión a la base de datos
 // Creando una instancia a la conexion de la DB
 const mongooseODM = new MongooseOdm(configKeys.mongoUrl);
-// Ejecutar la conexion a la BD
+// Ejecutar la conexion a la Bd
 // Crear una IIFE para crear un ambito asincrono
-// Que me permita usar async await
+// que me permita usar async await
 (async () => {
-  // Ejecutamos le metodo de conexion 
+  // Ejecutamos le metodo de conexion
   const connectionResult = await mongooseODM.connect();
-  // Checamos si hay error 
+  // Checamos si hay error
   if (connectionResult) {
-  // Si conecto correctamente a la base de datos
-  logger.info('✅ Conexion a la BD exitosa 🎌');
-} else {
-  logger.error('🥀 No se conecto a la base de dato');
-}
+    // Si conecto correctamente a la base de datos
+    logger.info('✅ Conexion a la BD exitosa 🛸');
+  } else {
+    logger.error('🎌 No se conecto a la base de datos');
+  }
 })();
+
 // view engine setup
 // Configura el motor de plantillas
 configTemplateEngine(app);
-:
+
 // Establezco Middelware
 app.use(morgan('dev', { stream: logger.stream }));
 // Middleware para parsear a json la peticion
--- server/domains/home/homeController.js
+++ server/domains/home/homeController.js
@@ -1,25 +1,25 @@
+// Cargando las variables de entorno
+import configKeys from '../../config/configKeys';
+
 // Creando los Actions Methods
 // GET "/"
-
-import configKeys from "../../config/configKeys";
-
 // GET "/index"
 const home = (req, res) => {
   // 1. Generando el view-model
   const viewModel = {
     title: 'Express',
-    author: 'Angel Mora Jimenez',
+    author: 'Rodriguez Rojas Marlene 🌸',
   };
 
   // 2. Madamos a generar la vista con el Template Engine
   res.render('home/home', viewModel);
 };
 
-//GET "/about"
+// GET "/about"
 const about = (req, res) => {
   const viewModel = {
     appVersion: configKeys.appVersion,
-  }
+  };
   res.render('home/about', viewModel);
 };
 
